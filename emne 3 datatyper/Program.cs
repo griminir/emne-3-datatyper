5b17821f6fc1662ec82b259bbde62cc83135a4e7
griminir <viktor.elliot.king@gmail.com>
2024-04-25 11:37:23 +0200

    Add project files.

---
+++ emne 3 datatyper/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//datatyper oppgave 1
+int wholeNumber = 1;
+long wholeNumber2 = 2;
+float decimalNumber = 3.11F;
+decimal pointNumber = 3.1111M;
+double decimalNumber2 = 3.14; //kan også sette en D back men trengs ikke
+string tekst = "hei";
+char bokstav = 'd';
+bool maybe = true;
+
+//datatyper oppgave 2
+int a = 5;
+decimal b = 3;
+decimal sum = a + b; //bruker den strengest
